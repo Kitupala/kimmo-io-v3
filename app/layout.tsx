ba857173dff1f57892cb8984a05c4d1a5a50e425
Kitupala <kimmo.tuupanen@gmail.com>
2025-06-25 12:00:45 +0300

    Integrate `LenisProvider` for smooth scrolling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,12 @@
 import React from "react";
-import type { Metadata } from "next";
-import { everett } from "./fonts";
-import "./globals.css";
+import "@/app/globals.css";
+import { everett, robotoMono } from "./fonts";
 
+import type { Metadata } from "next";
 import { PrismicPreview } from "@prismicio/next";
 import { repositoryName } from "@/prismicio";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-//
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+import LenisProvider from "@/app/components/LenisProvider";
 
 export const metadata: Metadata = {
   title: "kimmo.io",
@@ -28,7 +20,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${everett.variable} antialiased`}>{children}</body>
+      <body
+        className={`${everett.variable} ${robotoMono.variable} antialiased`}
+      >
+        <LenisProvider>{children}</LenisProvider>
+      </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
