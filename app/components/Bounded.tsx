09b3f671ff6641b342fbc19dc1f2925f0f2b21c7
Kitupala <kimmo.tuupanen@gmail.com>
2025-08-26 16:44:58 +0300

    Adjust max-width styling

--- app/components/Bounded.tsx
+++ app/components/Bounded.tsx
@@ -15,7 +15,9 @@ export const Bounded = ({
 }: BoundedProps) => {
   return (
     <Comp className={cn("px-4 first:pt-10 md:px-6", className)} {...restProps}>
-      <div className="mx-auto flex w-full max-w-4xl flex-col">{children}</div>
+      <div className="mx-auto flex w-full max-w-[919px] flex-col">
+        {children}
+      </div>
     </Comp>
   );
 };
