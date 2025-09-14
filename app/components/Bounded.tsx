a7e1a2628312c57c2b6c2e7d2f9fcdcdecba5637
Kitupala <kimmo.tuupanen@gmail.com>
2025-09-14 13:04:34 +0300

    Remove unused code

--- app/components/Bounded.tsx
+++ app/components/Bounded.tsx
@@ -1,27 +1,3 @@
-// import { cn } from "@/app/lib/utils";
-// import React from "react";
-//
-// type BoundedProps = {
-//   as?: React.ElementType;
-//   className?: string;
-//   children: React.ReactNode;
-// };
-//
-// export const Bounded = ({
-//   as: Comp = "section",
-//   className,
-//   children,
-//   ...restProps
-// }: BoundedProps) => {
-//   return (
-//     <Comp className={cn("px-4 first:pt-10 md:px-6", className)} {...restProps}>
-//       <div className="mx-auto flex w-full max-w-[919px] flex-col">
-//         {children}
-//       </div>
-//     </Comp>
-//   );
-// };
-
 import { cn } from "@/app/lib/utils";
 import React from "react";
 
